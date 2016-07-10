7ea9e8bc777d13fe53decf93860b4da2d1c29bd8
Oleg-Luchnikov <oleg19931208@gmail.com>
2016-07-10 15:02:45 +0300

    new project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+  var mySwiper = new Swiper ('.help-wrapper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    slidesPerView: 4,
+    spaceBetween:20
+  });
